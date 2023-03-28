c6bb44f5baf149e8d28e64f18d3a456353d5fdb7
James Sgarella <Jimbo8702@gmail.com>
2023-03-28 16:49:29 -0400

    eventDispatcher decorator, mailer service, types-express

---
+++ src/services/auth.ts
@@ -0,0 +1,18 @@
+import { Service, Inject } from 'typedi';
+import jwt from 'jsonwebtoken';
+import MailerService from './mailer';
+import config from '@/config';
+import argon2 from 'argon2';
+import { randomBytes } from 'crypto';
+import { IUser, IUserInputDTO } from '@/interfaces/IUser';
+import { EventDispatcher, EventDispatcherInterface } from '@/decorators/eventDispatcher';
+
+@Service()
+export default class AuthService {
+  constructor(
+    @Inject('userModel') private userModel: Models.UserModel,
+    private mailer: MailerService,
+    @Inject('logger') private logger,
+    @EventDispatcher() private eventDispatcher: EventDispatcherInterface,
+  ) {}
+}
